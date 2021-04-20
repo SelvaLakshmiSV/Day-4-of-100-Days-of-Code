2e6a1c8b33996c5c33c94c199ff033d92a779516
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-04-20 09:57:04 +0530

    Update Arithmetic operators.c

--- Arithmetic operators.c
+++ Arithmetic operators.c
@@ -13,3 +13,12 @@ int main()
     printf("Remainder when %d divide by %d is %d . \n10",a,b,a%b);
     return 0;
 }
+/*Output
+Enter a : 10
+Enter b : 2
+Addition of 10 and 2 is 12 .
+Subraction of 10 and 2 is 8 .
+Multiplication of 10 and 2 is 20 .
+Division of 10 and 2 is 5.
+Remainder when 10 divide by 2 is 0 .
+*/
